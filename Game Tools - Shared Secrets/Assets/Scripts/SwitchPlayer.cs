8624300eb493d4ee1f8b2d39ae74f8fc1861b81a
MrPineappleCB <c24405434@myTUDublin.ie>
2025-04-06 21:10:32 +0100

    work please

---
+++ Game Tools - Shared Secrets/Assets/Scripts/SwitchPlayer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayer : MonoBehaviour
+{
+    public GameObject pic;
+
+    // AudioManager audioManager;
+
+    // private void Awake()
+    // {
+    //audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    // }
+
+    void Start()
+    {
+        //audioManager.PlaySFX(audioManager.Success);
+        //audioManager.PlaySFX(audioManager.Success);
+    }
+
+    public void Trigger()
+    {
+        if (pic.activeInHierarchy == false)
+        {
+            pic.SetActive(true);
+            //audioManager.PlaySFX(audioManager.PenClick);
+        }
+        else
+        {
+            pic.SetActive(false);
+            //audioManager.PlaySFX(audioManager.PenClick);
+        }
+    }
+
+    public GameObject pic2;
+
+    public void Trigger2()
+    {
+        pic2.SetActive(false);
+        if (pic.activeInHierarchy == false)
+        {
+            pic2.SetActive(true);
+
+        }
+        else
+        {
+            pic2.SetActive(false);
+        }
+    }
+
+}
